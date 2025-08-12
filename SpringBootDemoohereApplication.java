77c8f2d351dada2b4e76e0acf1f3689cfe2bb2e8
Ranit Ghosh <ghoshranit25@gmail.com>
2025-08-12 12:19:38 +0530

    Main class uploaded here

---
+++ SpringBootDemoohereApplication.java
@@ -0,0 +1,13 @@
+package com.ranit.isback.SpringBootDemoohere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoohereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoohereApplication.class, args);
+	}
+
+}
